2dd522ae8706fa35ce5eb645b8d72df41126add2
axwii <axwi0001@stud.kea.dk>
2023-12-10 22:29:32 +0100

    Resturant button overview on takeaway

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ window.addEventListener("load", test);
 function test() {
   console.log("show test");
 }
+
+let btnContainer = document.getElementById('btn_container');
+let activeButton = null;
+
+btnContainer.addEventListener('click', function(event) {
+  if (event.target.classList.contains('secondary-btn')) {
+    // Check if the clicked element has the 'secondary-btn' class
+    if (activeButton) {
+      activeButton.classList.remove('active');
+    }
+    event.target.classList.add('active');
+    activeButton = event.target;
+  }
+});
