493d2b99dc0538f3ca793a5e4110e5800ee68c59
axwii <axwi0001@stud.kea.dk>
2023-12-11 02:42:48 +0100

    Made takeaway into JS script + cart icon

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function test() {
   console.log("show test");
 }
 
+// Active button on click - active on takeaway page and resturant page
 let btnContainer = document.getElementById('btn_container');
 let activeButton = null;
 
@@ -17,3 +18,36 @@ btnContainer.addEventListener('click', function(event) {
     activeButton = event.target;
   }
 });
+
+// takeaway page
+function createProductContainer(title, description, imgSrc, altText, price) {
+  const container = document.createElement('div');
+  container.className = 'product_container';
+
+  container.innerHTML = `
+    <h4 class="product_title">${title}</h4>
+    <p class="product_des">${description}</p>
+    <div class="product_img_container">
+      <img class="product_img" src="${imgSrc}" alt="${altText}">
+    </div>
+    <p class="product_prize">${price} kr.</p>
+    <button class="primary-btn">
+      <img src="img/cart.svg" alt="cart icon">
+      Put i kurv
+    </button>
+  `;
+
+  return container;
+}
+
+const snacksContainer = document.getElementById('snacksContainer');
+const saladsContainer = document.getElementById('saladsContainer');
+
+// Add snacks
+snacksContainer.appendChild(createProductContainer('Peanuts', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'img/menu/Grilled_Eggplants.avif', 'Snack 1', 40));
+snacksContainer.appendChild(createProductContainer('Shrip chips', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'img/menu/ruller.avif', 'Snack 2', 45));
+snacksContainer.appendChild(createProductContainer('Wontons', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'img/menu/Fried_Tofu-Sticks.png', 'Snack 3', 45));
+
+// Add salads
+saladsContainer.appendChild(createProductContainer('Broccoli thai kale', 'Ristet broccoli og thai kale med hvidløg. VG.', 'img/menu/salad.avif', 'Side 1', 65));
+saladsContainer.appendChild(createProductContainer('Cucumber salad', 'Traditionel, thailandsk agurkesalat, Tam Taeng, med thaibønner og thaiauberginer. GF, H', 'img/menu/ruller.avif', 'Side 2', 65));
